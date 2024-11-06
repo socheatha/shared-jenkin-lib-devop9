bab7bd4209743247587dfb0c81c12305c6bb56dd
KEO KAY <keokay888@gmail.com>
2024-11-06 12:30:17 +0000

    adding sonarqueb sacnning for reactjs

---
+++ vars/scanReactJsSonarqube.groovy
@@ -0,0 +1,21 @@
+def call(String projectName, String projectKey , String projectVersion){
+  
+         withSonarQubeEnv(credentialsId: 'SONARQUBE-TOKEN', installationName: 'sonarqube-server') {
+                script{
+                
+                //  def projectKey = 'reactjs-devops8-template' 
+                //     def projectName = 'Reactjs DevOps8 template'  // Replace with your project name
+                //     def projectVersion = '1.0.0'  // Replace with your project version
+                //             // sh "${scannerHome}/bin/sonar-scanner"
+                    sh """
+                    ${scannerHome}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName="${projectName}" \
+                    -Dsonar.projectVersion=${projectVersion} \
+                     """   
+                        
+                        }
+            }
+
+    
+}
