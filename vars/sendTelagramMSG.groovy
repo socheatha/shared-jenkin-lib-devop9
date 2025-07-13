acee09f987aeec133dada7b78e045cb9fef663bb
socheatha <34051351+socheatha@users.noreply.github.com>
2025-07-13 16:47:08 +0700

    ---

--- vars/sendTelagramMSG.groovy
+++ vars/sendTelagramMSG.groovy
@@ -1,9 +1,11 @@
-def call(String message, String token , String chatId) {
-    // uppgrade to use Markdown version instead 
-    def encodedMessage = URLEncoder.encode(message, "UTF-8")
-    sh """
-        curl -s -X POST https://api.telegram.org/bot${token}/sendMessage \\
-        -d chat_id=${chatId} \\
-        -d text="${encodedMessage}" > /dev/null
-    """
+def call(String message) {
+    withCredentials([usernamePassword(credentialsId: 'TELEGRAM_BOT', passwordVariable: 'TOKEN', usernameVariable: 'CHAT_ID')]) {
+        // uppgrade to use Markdown version instead 
+        def encodedMessage = URLEncoder.encode(message, "UTF-8")
+        sh """
+            curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage \\
+            -d chat_id=${CHAT_ID} \\
+             -d text="${encodedMessage}" > /dev/null
+        """
+    }
 }
